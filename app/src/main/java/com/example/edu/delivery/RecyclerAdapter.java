7caf876b5de34c264b8e0f0915ae04617c4cc6c3
edujav1924 <edujav22@gmail.com>
2018-04-10 22:27:47 -0400

    con button listener para cada reciclerview

--- app/src/main/java/com/example/edu/delivery/RecyclerAdapter.java
+++ app/src/main/java/com/example/edu/delivery/RecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.florent37.expansionpanel.ExpansionLayout;
@@ -16,7 +17,7 @@ import java.util.List;
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
     private final List<String> list = new ArrayList<>();
     private static final int LAYOUT = R.layout.reciclercell;
-    private String textos = "";
+    private String textos,textos2 = "";
     private final ExpansionLayoutCollection expansionsCollection = new ExpansionLayoutCollection();
 
     public void setItems(List<String> producto) {
@@ -38,15 +39,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
     @Override
     public void onBindViewHolder(RecyclerHolder holder, int position) {
         for (int i=0;i<list.size();i++){
-            textos = textos + "\uD83C\uDF73 \uD83C\uDF78"+list.get(i)+"\n";
-            Log.e("fdfd",textos);
+            textos = textos + "  \uD83C\uDF5D"+list.get(i)+"\n";
+            textos2 = textos2 + "  \uD83C\uDF79"+list.get(i)+"\n";
         }
         TextView text = holder.itemView.findViewById(R.id.hola);
         text.setText(list.get(position));
         TextView content = holder.itemView.findViewById(R.id.content);
         content.setText(textos);
+        TextView c = holder.itemView.findViewById(R.id.bebida);
+        c.setText(textos2);
         textos = "";
-        expansionsCollection.add(holder.getExpansionLayout());
+        textos2 = "";
+
+        expansionsCollection.add(holder.getExpansionLayout()).add(holder.getExpansionLayout());
     }
 
     @Override
@@ -62,7 +67,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Recycl
             expansionLayout.addListener(new ExpansionLayout.Listener() {
                 @Override
                 public void onExpansionChanged(ExpansionLayout expansionLayout, boolean expanded) {
-                    Log.e("ssfd", String.valueOf(getAdapterPosition()));
+
+                }
+            });
+            Button btn = itemView.findViewById(R.id.btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.e("ir a", String.valueOf(getAdapterPosition()));
                 }
             });
         }
