139735fd3c1a35551b64bd3d0ccc6cdd1818d000
edujav1924 <edujav22@gmail.com>
2018-04-16 12:37:28 -0400

    antes de easyview choputa

---
+++ app/src/main/java/com/example/edu/delivery/init.java
@@ -0,0 +1,13 @@
+package com.example.edu.delivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class init extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
