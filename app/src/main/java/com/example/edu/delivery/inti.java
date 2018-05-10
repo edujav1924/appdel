072bcfb71e03e7fcdae97b851c6acb8e3078804f
edujav1924 <edujav22@gmail.com>
2018-05-10 15:41:13 -0400

    10 mayo cabios

--- app/src/main/java/com/example/edu/delivery/inti.java
+++ app/src/main/java/com/example/edu/delivery/inti.java
@@ -2,33 +2,31 @@ package com.example.edu.delivery;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
-import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkError;
@@ -40,11 +38,10 @@ import com.android.volley.Response;
 import com.android.volley.ServerError;
 import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.github.ybq.android.spinkit.style.DoubleBounce;
 import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -52,18 +49,10 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
 import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.medialablk.easygifview.EasyGifView;
+
 import com.shashank.sony.fancydialoglib.Animation;
 import com.shashank.sony.fancydialoglib.FancyAlertDialog;
 import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
@@ -90,28 +79,30 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
 public class inti extends AppCompatActivity {
-    private static final int REQUEST_CHECK_SETTINGS = 100;
     private FusedLocationProviderClient mFusedLocationClient;
     LocationRequest mLocationRequest;
     TextView texto_init, locationnet;
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     private LocationCallback mLocationCallback;
-    private boolean mRequestingLocationUpdates = true;
     LocationManager manager;
-    ImageView load;
     private boolean band = false;
     Drawable drawable;
     Boolean flag = false;
+    View spin;
+    Button last_ubication;
+    private String base_latitud,base_longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inti);
-
-        load = findViewById(R.id.loading);
-        load.setVisibility(View.GONE);
+        spin = findViewById(R.id.spin_kit);
+        spin.setVisibility(View.GONE);
         texto_init = findViewById(R.id.text_init);
         locationnet = findViewById(R.id.locationnet);
+        last_ubication = findViewById(R.id.lastu);
+        last_ubication.setVisibility(View.GONE);
+        Log.e("leleef","df");
         manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
@@ -122,6 +113,11 @@ public class inti extends AppCompatActivity {
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         } else {
+            Log.e("status", "0");
+
+            basedatos mDbHelper = new basedatos(inti.this);
+            Log.e("leerubicatiob","si");
+            leerubicacion(mDbHelper);
             mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         }
 
@@ -129,24 +125,25 @@ public class inti extends AppCompatActivity {
 
     private void errors(String a) {
         texto_init.setText(a);
-
+        locationnet.setVisibility(View.GONE);
+        spin.setVisibility(View.GONE);
 
     }
 
     public void nubication(View view) {
         getlocation();
-        onload();
     }
 
     public void lubication(View view) {
-        basedatos mDbHelper = new basedatos(inti.this);
-        leerubicacion(mDbHelper);
+        spin.setVisibility(View.VISIBLE);
+        getdata(base_latitud,base_longitud);
     }
-
+    private void texto_inicio(String texto){
+        texto_init.setText(texto);
+    }
     private void leerubicacion(basedatos mDbHelper) {
 
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        Log.e("database", String.valueOf(db));
 // Define a projection that specifies which columns from the database
 // you will actually use after this query.
         String[] projection = {
@@ -158,44 +155,27 @@ public class inti extends AppCompatActivity {
         String selection = basedatos.FeedEntry._ID +"=?";
         String[] selectionArgs = { "1" };
 
-        //Cursor c = db.rawQuery(" SELECT * FROM entry Where 'basedatos.FeedEntry._ID=0' ", null);
         Cursor c = db.query(basedatos.FeedEntry.TABLE_NAME,projection,selection, selectionArgs,null,null,null);
         c.moveToFirst();
-        Log.e("count", String.valueOf(c.getCount()));
+        Log.e("valor de C", String.valueOf(c.getCount()));
         if(c.getCount()>0){
             if(!c.getString(1).equals("0")){
-                //getdata(c.getString(0), c.getString(1));
-                Log.e("id1",c.getString(1) +" "+c.getString(2));
-                getdata(c.getString(2),c.getString(1));
+                base_latitud = c.getString(2);
+                base_longitud = c.getString(1);
+                //getdata(c.getString(2),c.getString(1));
+                last_ubication.setVisibility(View.VISIBLE);
             }
-
+        }else {
+            Log.e("aca","aca");
         }
         db.close();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.DONUT)
-    private void onload() {
-        drawable = load.getDrawable();
-        if (drawable instanceof Animatable) {
-            Log.e("entre", "onload");
-            ((Animatable) drawable).start();
-            load.setVisibility(View.VISIBLE);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.DONUT)
-    private void offload() {
-        drawable = load.getDrawable();
-        if (drawable instanceof Animatable) {
-            Log.e("entre", "offload");
-            ((Animatable) drawable).stop();
-
-        }
-        ((Animatable) drawable).stop();
-        load.setVisibility(View.GONE);
-    }
 
     private void getlocation() {
+        texto_inicio("Obteniendo localizacion");
+        spin.setVisibility(View.VISIBLE);
+
         if (!manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) && !flag) {
             showAlert();
         }else {
@@ -206,13 +186,11 @@ public class inti extends AppCompatActivity {
                 @Override
                 public void onLocationResult(LocationResult locationResult) {
                     if (locationResult == null) {
-                        Log.e("loation", "null");
 
                     }
                     for (Location location : locationResult.getLocations()) {
                         String latitud = location.convert(location.getLatitude(), 0);
                         String longitud = location.convert(location.getLongitude(), 0);
-                        Log.e("customlatitud", String.valueOf(latitud.indexOf(',')));
                         if (latitud.indexOf(',')>=0){
                             latitud = latitud.replace(',', '.');
                             longitud = longitud.replace(',', '.');
@@ -220,10 +198,9 @@ public class inti extends AppCompatActivity {
 
                         locationnet.setText("aproximando a: " + String.valueOf(location.getAccuracy()) + " metros");
                         if (location.getAccuracy() <= 25.0) {
-                            Log.e("loation", String.valueOf(location.getAccuracy()));
-                            texto_init.setText("obteniendo datos ..");
-                            getdata(latitud, longitud);
                             stopLocationUpdates();
+                            getdata(latitud, longitud);
+
                         }
                     }
                 }
@@ -232,7 +209,6 @@ public class inti extends AppCompatActivity {
             };
             createLocationRequest();
             startLocationUpdates();
-            texto_init.setText("obteniendo ubicacion");
         }
 
     }
@@ -240,22 +216,19 @@ public class inti extends AppCompatActivity {
     @Override
     public void onPause() {
         super.onPause();  // Always call the superclass method first
-        Log.e("onremis","pause");
 
 
     }
     @Override
     public void onRestart() {
         super.onRestart();  // Always call the superclass method first
-        Log.e("onremis","restar");
 
 
     }
     @RequiresApi(api = Build.VERSION_CODES.DONUT)
     @Override
     public void onResume() {
-        super.onResume();  // Always call the superclass method first
-        Log.e("onremis","onresume");
+        super.onResume();
         if (!manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) && !flag) {
             showAlert();
         }
@@ -280,10 +253,10 @@ public class inti extends AppCompatActivity {
         Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
         task.addOnFailureListener(this, new OnFailureListener() {
             @RequiresApi(api = Build.VERSION_CODES.DONUT)
+
             @Override
             public void onFailure(@NonNull Exception e) {
                 if (e instanceof ResolvableApiException) {
-                    Log.e("gpssd","hola");
                     try {
                         // Show the dialog by calling startResolutionForResult(),
                         // and check the result in onActivityResult().
@@ -321,12 +294,10 @@ public class inti extends AppCompatActivity {
             case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.e("status","3");
                     mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
                     flag=false;
                 }
                     else {
-                    Log.e("status","4");
                     finish();
 
                 }
@@ -354,7 +325,6 @@ public class inti extends AppCompatActivity {
         try {
             sc = SSLContext.getInstance("SSL");
             sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            Log.d("test_disable", "OK========");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         } catch (KeyManagementException e) {
@@ -375,10 +345,10 @@ public class inti extends AppCompatActivity {
     }
 
     private void getdata(final String latitudcel, final String longitudcel) {
+        texto_inicio("obteniendo datos");
         Disable_Certificate_Validation_Java_SSL_Connections();
-
         RequestQueue queue2 = Volley.newRequestQueue(this);
-        String url ="https://192.168.43.158:443/empresa.json";
+        String url ="https://delivery.simplelectronica.com:443/empresa.json";
         StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @RequiresApi(api = Build.VERSION_CODES.DONUT)
@@ -404,7 +374,6 @@ public class inti extends AppCompatActivity {
                             e.printStackTrace();
                            errors("Hubo un error al recibir datos del servidor");
                             Log.e("Error1","Error1");
-                            offload();
                         }
                     }
                 }, new Response.ErrorListener() {
@@ -427,14 +396,12 @@ public class inti extends AppCompatActivity {
 
                     errors("Error de formato de datos");
                 }
-                offload();
             }
         });
         queue2.add(stringRequest2);
     }
 
     private void getdirections(final JSONArray jsono, List<String> latitud, List<String> longitud, final String latitudcel, final String longitudcel) {
-        texto_init.setText("obteniendo localizacion...");
 
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://maps.googleapis.com/maps/api/distancematrix/json?units=meter&origins="+latitudcel+","+longitudcel+"&destinations=";
@@ -466,12 +433,10 @@ public class inti extends AppCompatActivity {
                                 tiempo.add(aux3.getString("text"));
                                 //Log.e("get", aux3.getString("text"));
                             }
-                            texto_init.setText("Finalizado");
                             desarrollo(direcciones,distancias,tiempo,jsono,longitudcel,latitudcel);
                         } catch (JSONException e) {
                             Log.e("error", String.valueOf(e));
                             e.printStackTrace();
-                            offload();
                             texto_init.setText("Hubo un error al recibir datos de localizacion");
                         }
                     }
@@ -479,7 +444,6 @@ public class inti extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 texto_init.setText("no fue posible obtener datos de localizacion");
-                offload();
             }
         });
         queue.add(stringRequest);
